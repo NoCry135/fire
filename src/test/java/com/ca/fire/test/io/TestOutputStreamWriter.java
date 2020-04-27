37d55333c6b804d2bdf1e65fd07f6c2aea2c8736
liwen135 <1396968962@qq.com>
2020-04-27 23:13:12 +0800

    FileUtils.readFileToString

--- src/test/java/com/ca/fire/test/io/TestOutputStreamWriter.java
+++ src/test/java/com/ca/fire/test/io/TestOutputStreamWriter.java
@@ -1,8 +1,17 @@
 package com.ca.fire.test.io;
 
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
 import java.io.*;
 
 public class TestOutputStreamWriter {
+
+    @Test
+    public void test01() throws IOException {
+        String string = FileUtils.readFileToString(new File("info1.txt"));
+        System.out.println(string);
+    }
 
     public static void main(String[] args) throws Exception {
         //test1("myjava/info1.txt");
